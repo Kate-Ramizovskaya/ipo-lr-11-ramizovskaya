4c2b1a986f83d974054c57fa0bd3fe5080f074c1
Vlad <vladworking5@gmail.com>
2024-12-16 18:20:52 +0300

    some change in transport

--- transport/vehicle.py
+++ transport/vehicle.py
@@ -3,13 +3,19 @@ from .client import Client
 
 class Vehicle:
     def __init__(self, capacity):
-        self.vehicle_id = str(uuid.uuid4())
-        self.capacity = capacity
-        self.current_load = 0
-        self.clients_list = []
+        if isinstance(capacity, (int, float)) and capacity > 0:
+            self.vehicle_id = str(uuid.uuid4())
+            self.capacity = capacity
+            self.current_load = 0
+            self.clients_list = []
+        else:
+            raise ValueError("Грузоподъемность должна быть положительным числом!")
 
     def can_load(self, client):
-        return self.current_load + client.cargo_weight <= self.capacity
+        if isinstance(client.cargo_weight, (int, float)) and client.cargo_weight > 0:
+            return self.current_load + client.cargo_weight <= self.capacity
+        else:
+            raise ValueError("Вес груза клиента должен быть положительным числом!")
 
     def load_cargo(self, client):
         if self.can_load(client):
